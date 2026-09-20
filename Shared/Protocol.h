ce0d635496e1de0832908286447b792b0844e401
ItsStillSunny <huynhhieunghiadn@gmail.com>
2026-09-20 21:02:12 +0700

    Add enum MessageType to Protocol,h (unfinished)

--- Shared/Protocol.h
+++ Shared/Protocol.h
@@ -2,5 +2,19 @@
 
 namespace Protocol
 {
-    
+    enum MessageType{
+        MSG_PING = 1,
+        MSG_PONG,
+        MSG_DISCONNECT,
+
+        MSG_VIDEO_FRAME,
+        MSG_AUDIO_CHUNK,
+
+        MSG_MOUSE_EVENT,
+        MSG_KEY_EVENT,
+
+        MSG_SYS_INFO,
+        MSG_PROCESS_LIST,
+        MSG_KILL_PROCESS,
+    };
 } 
